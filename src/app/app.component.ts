b3f262ef4b7bd0fb18e63a3c5d1b7688cdea6839
hardikt1793 <hardikt1793@gmail.com>
2025-01-01 17:41:32 +0530

    code refactor
    formatting, proper comments, assigning proper types/interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { NodeModel } from './model/node.model';
+import { Component } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+import { NodeModel } from "./model/node.model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-
-
 export class AppComponent {
-  folderName = new FormControl('', [Validators.required]);
+  // to track the entering name control
+  folderName = new FormControl("", [Validators.required]);
 
   // store the tree structure data
   folderStructureData: NodeModel[] = [];
@@ -19,24 +18,23 @@ export class AppComponent {
   showHideFolderSection = false;
 
   /**
-   * It's for checking form validation and push new data.
-   * 
+   * It's for checking form validation and add new folder to root.
+   * @returns
    */
-  addFolderToRoot() {
+  addFolderToRoot(): void {
     if (this.folderName.invalid) {
       this.folderName.markAsTouched();
       return;
     }
-    
+
     this.folderStructureData.push({
-      type: 'folder',
+      type: "folder",
       name: this.folderName.value,
       children: [],
       id: Math.random().toString(36).substring(2, 9),
-    })
+    });
 
     this.folderName.reset();
-
     this.showHideFolderSection = !this.showHideFolderSection;
   }
 }
--- src/app/tree-item/tree-item.component.ts
+++ src/app/tree-item/tree-item.component.ts
@@ -1,33 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { NodeModel } from '../model/node.model';
+import { Component, Input } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+import { NodeModel } from "../model/node.model";
 
 @Component({
-  selector: 'app-tree-item',
-  templateUrl: './tree-item.component.html',
-  styleUrls: ['./tree-item.component.scss']
+  selector: "app-tree-item",
+  templateUrl: "./tree-item.component.html",
+  styleUrls: ["./tree-item.component.scss"],
 })
-export class TreeItemComponent implements OnInit {
+export class TreeItemComponent {
   // This is to Get child tree structure from parent component
-  @Input() children: any;
+  @Input() children!: NodeModel[];
 
-  folderName = new FormControl('', [Validators.required]);
-    
-  constructor() { 
-  }
-
-  ngOnInit(): void {
-  }
+  // to track the entering name control
+  folderName = new FormControl("", [Validators.required]);
 
   /**
    * Adding new item to the children array.
-   * 
+   *
    * @param item - Item object
    */
-  addChild(item: NodeModel) {
-    return item.children?.push({
-      type: 'unset',
-      name: '',
+  addChild(item: NodeModel): void {
+    item.children?.push({
+      type: "unset",
+      name: "",
       children: [],
       id: Math.random().toString(36).substring(2, 9),
     });
@@ -35,31 +30,25 @@ export class TreeItemComponent implements OnInit {
 
   /**
    * It's for checking form validation and add item name.
-   * 
+   *
    * @param item - Item object
    */
-  addName(item: NodeModel) {
+  addName(item: NodeModel): void {
     if (this.folderName.invalid) {
       this.folderName.markAsTouched();
       return;
     }
-    
-    item['name'] = this.folderName.value;
-
+    item["name"] = this.folderName.value;
     this.folderName.reset();
-
-    return item;
   }
 
   /**
    * It's to delete item from children array.
-   * 
+   *
    * @param children - Items array
    * @param index - Item index
    */
-  deleteChild(children: NodeModel[], index: number) {
+  deleteChild(children: NodeModel[], index: number): void {
     children.splice(index, 1);
-
-    return children;
   }
 }
